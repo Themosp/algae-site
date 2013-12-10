c54b6bfcc1f8ae9eb7a352d7cff9f8de129d00d0
Eric Cohen <eirc.eirc@gmail.com>
2013-12-10 20:33:47 +0200

    Initial commit of `rails new algae_site --skip-bundle --database=postgresql --skip-test-unit`.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlgaeSite::Application.config.secret_key_base = 'a06717a8b514e00ff0f5b2118df1640afefa68e02b2b0f150dfcf9845374db37eabc2c5b817956d9422d8bf3928f0406e88f625d0c125924a57418b747fa0d18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlgaeSite::Application.config.session_store :cookie_store, key: '_algae_site_session'
