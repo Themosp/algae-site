24a8eda39747e0bad89d3ab67a346718be0c286e
Eric Cohen <eirc.eirc@gmail.com>
2014-03-29 19:30:24 +0200

    Add flag images for languages along with names so they are more visible.
    Flag icons taken from http://antibakteriell.deviantart.com/art/Flag-Icons-Europe-166536006 and http://antibakteriell.deviantart.com/art/Flag-Icons-Asia-166549656 . As linked by http://www.hongkiat.com/blog/world-flags-iconsets/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
         ar: 'Arabic'
     }[locale]
   end
+
+  def language_flag(locale)
+    {
+        el: 'flags/Greece.png',
+        en: 'flags/UnitedKingdom.png',
+        de: 'flags/Germany.png',
+        it: 'flags/Italy.png',
+        es: 'flags/Spain.png',
+        ar: 'flags/SaudiArabia.png'
+    }[locale]
+  end
 end
