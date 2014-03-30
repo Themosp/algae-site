8e95bb203648f7468840a9ff6c0bf1f3f39d5a6e
Eric Cohen <eirc.eirc@gmail.com>
2014-03-30 16:43:08 +0300

    Default scope for uploads.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,4 +3,6 @@ class Upload < ActiveRecord::Base
 
   has_attached_file :file_attachment
   validates_attachment_content_type :file_attachment, :content_type => /.*/
+
+  default_scope { order(:id).includes(:translations) }
 end
